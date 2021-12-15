c2548268415083f969a4fc21655610a5df9afe08
AmnesiackY <hitsac2711@gmail.com>
2021-12-15 17:45:28 +0200

    add project

---
+++ Morze/Morze/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Morze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+            Console.WriteLine("Enter phrase for encription");
+            
+            string phrase = Console.ReadLine();
+            string final_morz = "";
+            foreach(char let in phrase)
+            {
+                final_morz += Transformation.encr_morze[let.ToString()] + " ";
+            }
+            Console.WriteLine(final_morz);
+            final_morz = final_morz.Trim();
+            
+            string time_unit = "";
+            foreach(char ch in final_morz)
+            {
+                time_unit += Transformation.time_units[ch];
+            }
+            time_unit = time_unit.Substring(0, time_unit.Length - 2);
+            Console.WriteLine("Time-unit code");
+            Console.WriteLine(time_unit);
+        }      
+    }
+}
---
+++ Morze/Morze/Transformation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Morze
+{
+    class Transformation
+    {
+        public static Dictionary<string, string> encr_morze = new Dictionary<string, string>
+            {
+                { "a", ". -   " },
+                { "b", "- . . .   " },
+                { "c", "- . - .   " },
+                { "d", "- . .   " },
+                { "e", ".   " },
+                { "f", ". . - .   " },
+                { "g", "- - .   " },
+                { "h", ". . . .   " },
+                { "i", ". .   " },
+                { "j", ". - - -   " },
+                { "k", "- . -   " },
+                { "l", ". - . .   " },
+                { "m", "- -   " },
+                { "n", "- .   " },
+                { "o", "- - -   " },
+                { "p", ". - - .   " },
+                { "q", "- - . -   " },
+                { "r", ". - .   " },
+                { "s", ". . .   " },
+                { "t", "- " },
+                { "u", ". . -   " },
+                { "v", ". . . -   " },
+                { "w", ". - -   " },
+                { "x", "- . . -   " },
+                { "y", "- . - -   " },
+                { "z", "- - . .   " },
+                { " ","      " }
+            };
+        public static Dictionary<char, string> time_units = new Dictionary<char, string>
+        {
+            { '.', "11" },
+            { '-', "111111" },
+            { ' ' , "00" }
+        };
+
+    }
+}
