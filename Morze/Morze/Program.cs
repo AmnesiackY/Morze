9add631967af41082a1e095337c67dafb0f4d6d0
AmnesiackY <hitsac2711@gmail.com>
2021-12-15 17:47:50 +0200

    addd Pravki_2 ver 1.2

--- Morze/Morze/Program.cs
+++ Morze/Morze/Program.cs
@@ -14,7 +14,7 @@ namespace Morze
             string final_morz = "";
             foreach(char let in phrase)
             {
-                final_morz += Transformation.encr_morze[let.ToString()] + " ";
+                final_morz += Transformation.encr_morze[let.ToString()];
             }
             Console.WriteLine(final_morz);
             final_morz = final_morz.Trim();
@@ -24,7 +24,6 @@ namespace Morze
             {
                 time_unit += Transformation.time_units[ch];
             }
-            time_unit = time_unit.Substring(0, time_unit.Length - 2);
             Console.WriteLine("Time-unit code");
             Console.WriteLine(time_unit);
         }      
